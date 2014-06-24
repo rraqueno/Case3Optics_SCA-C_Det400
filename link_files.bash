decd47d978839b6cbc500d3a44dc8392c722439c
U-homestar\rvrpci <rvrpci@homestar.(none)>
2014-06-24 11:15:28 -0400

    Output of this case has been sent to Nina for analysis (6/24/2014) ; Please enter the commit message for your changes. Lines starting ; with '#' will be ignored, and an empty message aborts the commit. ; ; Committer: U-homestar\rvrpci <rvrpci@homestar.(none)> ; ; On branch master ; Changes to be committed: ;   (use "git reset HEAD <file>..." to unstage) ; ;	modified:   B11.csv ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-flicker.hdr ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-flicker.img ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-sampled.hdr ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-sampled.img ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled-Ghost_Weights_Radiances.csv ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled-Ghost_Weights_Radiances.txt ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled-high_res_Ghost_Weights_Radiances.csv ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled-high_res_Ghost_Weights_Radiances.txt ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled.hdr ;	modified:   GOES-B11-L8-MODIS-geo-bias-water-unsampled.img ;	modified:   L8-MODIS.hdr ;	new file:   OpticsModelPerDet11_20140527_RITedits_Y_FLIPPEDCSV.csv ;	modified:   link_files.bash ;

--- link_files.bash
+++ link_files.bash
@@ -3,4 +3,4 @@ cp -uv MODIS/MOD11_L2-A2013126-1745geo_L8TOA_B11.hdr MODIS.img.hdr
 cp -uv ImageRadLatsLons_Column_1520_SCA-C_Det400_case3_126.img L8.img
 cp -uv ImageRadLatsLons_Column_1520_SCA-C_Det400_case3_126.hdr L8.hdr
 cp -uv ImageRadLatsLons_Column_1520_SCA-C_Det400_case3_126.bll L8.bll
-cp -uv B11_mar11_screened_USE_THIS_ONE_CSV.csv B11.csv
+cp -uv OpticsModelPerDet11_20140527_RITedits_Y_FLIPPEDCSV.csv B11.csv
